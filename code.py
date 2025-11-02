38c918293e33751a69327f9f94b5abf898faf415
ShedrachIkenna <Shedrach686@gmail.com>
2025-11-02 17:04:38 +0100

    Added step function description

--- code.py
+++ code.py
@@ -1,5 +1,11 @@
 import numpy as np 
 
-def step_function(x):
+# Step function 
+def step_function(x): 
+    """
+    Step function is the activation function used in perceptrons
+        - Its a binary classifier/decision maker 
+        - Think of it as a decision switch that decides whether or not a neuron should be activate(1) or not(0)
+    """
     return 1 if x >= 0 else 0 
 
